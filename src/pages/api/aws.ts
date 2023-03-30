afbdc2813c57fd83f2503adce0e05a7dbf8022d7
Jazim Abbas <jazimabbas87@gmail.com>
2023-03-30 13:03:02 +0500

    change the redirect url

--- src/pages/api/aws.ts
+++ src/pages/api/aws.ts
@@ -7,8 +7,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
   try {
     const marketplaceToken = _validateReqBody(req.body);
-    const redirectURL = "/api/test?token=" + marketplaceToken;
-    
+    const redirectURL = "/marketplace-signup?token=" + marketplaceToken; // frontend signup page url
+
     res.redirect(302, redirectURL);
   } catch (err: any) {
     res.status(400).json({ message: err.message });
